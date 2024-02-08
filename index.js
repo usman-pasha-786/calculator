96a38253782dc1f06f098ca63fc08d0fdae8d23f
usman-pasha-786 <147035343+usman-pasha-786@users.noreply.github.com>
2024-02-08 19:30:49 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+do {
+    const answer = await inquirer.prompt([
+        {
+            type: "number",
+            name: "num1",
+            message: "Enter your first Number :"
+        },
+        {
+            type: "number",
+            name: "num2",
+            message: "Enter your second Number :"
+        },
+        {
+            type: "list",
+            name: "operator",
+            choices: ["+", "-", "*", "/"],
+            message: "Select your operator"
+        }
+    ]);
+    const { num1, num2, operator } = answer;
+    if (num1 && num2 && operator) {
+        let result = 0;
+        if (operator === "+") {
+            result = num1 + num2;
+        }
+        else if (operator === "-") {
+            result = num1 - num2;
+        }
+        else if (operator === "*") {
+            result = num1 * num2;
+        }
+        else if (operator === "/") {
+            result = num1 / num2;
+        }
+        console.log("Answer :", result);
+    }
+    else {
+        " Kindly Enter a valid operator";
+    }
+} while (true);
